cf20cef711bc8b1cbe90adc455510cfc7c346ef1
Leonid Sorkin <leonid01@sebo-tech.com>
2019-12-02 16:27:10 +0200

    1

--- BLE/BLE_Services.c
+++ BLE/BLE_Services.c
@@ -230,12 +230,6 @@ RESULT Serv_SendToHost(CHARACTERISTIC_ID CharId, uint8_t *pData, uint16_t DataLe
   
   default:
     return ERR_BLE_CHARACTERISTIC_ID;
-    params.handle = m_lbs.hFlashDatChar.value_handle;
-    break;
-    
-  
-  default:
-    return ERR_BLE_CHARACTERISTIC_ID;
     break;
   }
 
